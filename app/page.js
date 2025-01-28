19dd9f268d2728c7ee8fc38c25aa23e46a326bf8
Pedro Leite <pedrolte11@hotmail.com>
2025-01-27 21:16:27 -0300

    Primeiro commit

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Enviando para Github</h1>
+      <p>Atividade finalizada</p>
+    </div>
+  );
+}
